bdde787329f08d1c625da21cfb1bef0de3bb782a
Alessandro <nola.alessandro@iisgalvanimi.edu.it>
2026-02-06 10:28:01 +0000

    revisione

--- Nola-Alessandro-filaB/src/app/app.ts
+++ Nola-Alessandro-filaB/src/app/app.ts
@@ -1,11 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SecondComponentComponent } from './second-component/second-component.component';
+import { ThirdComponentComponent } from './third-component/third-component.component';
+import { FourthComponentComponent } from './fourth-component/fourth-component.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, 
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css', 
+  imports: [
+    RouterOutlet, 
+    SecondComponentComponent, 
+    ThirdComponentComponent, 
+    FourthComponentComponent
+  ],
 })
 export class App {
   protected readonly title = signal('Nola-Alessandro-filaB');
